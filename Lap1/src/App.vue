8f967c77bbaa1dcfcecc6990f1b1884da045007d
FhLk <paramat10042545@hotmail.com>
2022-01-25 12:45:53 +0700

    Basic syntax Vue

--- Lap1/src/App.vue
+++ Lap1/src/App.vue
@@ -1,21 +1,16 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+const msg="Wow!!!!!";
+const headingstyle="color:red"
+const imagePath='../public/images/logo.png'
 </script>
-
+ 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+<h1 v-bind:style="headingstyle">{{ msg }}</h1>
+<h2 :style="headingstyle">Year!!!!</h2>
+<img src="./assets/logo.png" />
+<img :src="imagePath" alt="" srcset="" />
 </template>
-
+ 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
