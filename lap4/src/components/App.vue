d8ab022381eff1206f79bd9153dbc56a8712b039
FhLk <paramat10042545@hotmail.com>
2022-02-11 19:23:36 +0700

    Add Lap4

---
+++ lap4/src/components/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+const redstyle={color:"red",'font-style': "italic"}
+</script>
+ 
+<template>
+<!-- Object syntax -->
+<p :style="redstyle">This is style with object syntax</p>
+<!-- <p :style="isError ? {color:red}:{color:blue}">This is Error message</p> -->
+<!-- array syntax -->
+<p :style="[redstyle]">This is Test style</p>
+</template>
+ 
+<style>
+.blueText{
+    color: blue
+}
+</style>
