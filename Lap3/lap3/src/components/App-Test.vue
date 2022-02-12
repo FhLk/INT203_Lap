a90eddb353f63a24b013182bad861b21547bced6
FhLk <paramat10042545@hotmail.com>
2022-02-12 11:13:49 +0700

    add midterm

---
+++ Lap3/lap3/src/components/App-Test.vue
@@ -0,0 +1,37 @@
+<script setup>
+import {ref} from 'vue'
+const text=ref('')
+const note=ref([]);
+const FilterNote = ref([])
+let isShow=ref(false);
+const addNote=()=>{
+          isShow.value=true
+          note.value.push(text.value)
+          text.value=''
+}
+const fil='';
+let isShowFilter =ref(false)
+const filter = (text)=>{
+          let value =new RegExp(String(text), 'i')
+          FilterNote.value = note.value.filter((t)=>t.match(value))
+          isShowFilter.value=true
+}
+</script>
+ 
+<template>
+<p>New Note : 
+          <input type="text" v-model="text" placeholder="Note..."/>
+          <button @click="addNote" style="cursor: pointer">Add Note</button>
+</p>
+<ul v-if="note!=''">
+          <li v-for="(value,index) in isShowFilter ? FilterNote : note " :key="index">{{value}}</li>
+</ul>
+<p v-if="isShow">Filter Note : 
+          <input type="text" v-model="fil" @keyup="filter(fil)" placeholder="Filter..."/>
+</p>
+
+</template>
+ 
+<style>
+
+</style>
---
+++ midterm/src/components/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+const myAccount={
+  accoutID:'12345',
+  accountName: 'FhLk',
+  balance: 20000
+}
+
+const profile={
+  name:'Paramat',
+  age:20,
+  sex: 'Female',
+  favorite:['swiming','Game',20,'BNK','CGM']
+}
+</script>
+ 
+<template>
+<ul>
+  <li v-for="(value,prop,index) in profile" :key="index">
+    {{prop}}:{{value}}
+  </li>
+</ul>
+</template>
+ 
+<style>
+
+</style>
