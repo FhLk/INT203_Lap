13e92034da7182c5cf9e85a668c074bb88722f3f
FhLk <paramat10042545@hotmail.com>
2022-02-15 13:08:58 +0700

    Finish midterm

--- Lap3/lap3/src/components/App.vue
+++ Lap3/lap3/src/components/App.vue
@@ -1,11 +1,46 @@
 <script setup>
-
+import {ref} from 'vue'
+const text=ref('')
+const note=ref([]);
+const FilterNote = ref([])
+let isShow=ref(false);
+const addNote=(t)=>{
+    if(t!=''){
+          isShow.value=true
+          note.value.push(text.value)
+          text.value=''
+    }
+}
+const fil='';
+let isShowFilter =ref(false)
+const filter = (text)=>{
+          let value =new RegExp(String(text), 'i')
+          FilterNote.value = note.value.filter((t)=>t.match(value))
+          isShowFilter.value=true
+}
 </script>
  
 <template>
+<p>New Note : 
+          <input type="text" v-model="text" @keyup.enter="addNote(text)" placeholder="Note..."/>
+          <button @click="addNote(text)" style="cursor: pointer">Add Note</button>
+</p>
+<ul class="bg-primary text-white" v-if="note!=''">
+          <li v-for="(value,index) in isShowFilter ? FilterNote : note " :key="index">{{value}}</li>
+</ul>
+<p v-if="isShow">Filter Note : 
+          <input type="text" v-model="fil" @keyup="filter(fil)" placeholder="Filter..."/>
+</p>
 
 </template>
  
 <style>
+.bg-primary{
+    background-color: antiquewhite;
+}
+
+.listNote2{
+    background-color: rgb(236, 129, 129);
+}
 
 </style>
--- midterm/src/components/App.vue
+++ midterm/src/components/App.vue
@@ -1,27 +1,97 @@
 <script setup>
 import { ref } from 'vue'
-const myAccount={
-  accoutID:'12345',
-  accountName: 'FhLk',
-  balance: 20000
+const randNumbers = ref([])
+const buttunIngrid=ref([true,true,true,true,true,true,true,true,true]);
+const listgrid=ref([])
+const error = ref(false);
+// generates random number in range (1-99)
+const generateNumber = () => {
+  let duplicate = false
+  const rand = Math.floor(Math.random() * 99 + 1)
+  duplicate = randNumbers.value.some((randNumber) => randNumber === rand)
+  duplicate ? generateNumber() : randNumbers.value.push(rand)
+}
+let lastNumber;
+const addNumber=(num)=>{
+  lastNumber=randNumbers.value[randNumbers.value.length-1]
+  if(listgrid.value.includes(lastNumber)){
+    error.value = true;
+    return
+  }
+  listgrid.value[num] = lastNumber;
+  buttunIngrid.value[num]=false;
+  error.value=false
 }
 
-const profile={
-  name:'Paramat',
-  age:20,
-  sex: 'Female',
-  favorite:['swiming','Game',20,'BNK','CGM']
+const Reset=()=>{
+  randNumbers.value=[];
+  buttunIngrid.value=[true,true,true,true,true,true,true,true,true];
+  listgrid.value=[];
+  error.value=false;
 }
+
+const CheckStyle=(number)=>{
+  if(listgrid.value.includes(number)){
+    return 'text-decoration-line: line-through; color:red'
+  }
+}
+
 </script>
- 
-<template>
-<ul>
-  <li v-for="(value,prop,index) in profile" :key="index">
-    {{prop}}:{{value}}
-  </li>
-</ul>
-</template>
- 
-<style>
 
+<template>
+  <div class="top-container">
+    <div class="action-button">
+      <button :disabled="randNumbers.length == 99 || !(listgrid.length<=9)" @click="generateNumber">Generate New Number</button>
+      <button @click="Reset">Reset</button>
+    </div>
+    <div class="error-message" v-show="error">
+      Sorry, Your number is already exist in the bingo sheet, please generate new number again!
+    </div>
+    <div class="grid-container">
+      <div class="grid-item" v-for="(grid,index) in 9" :key="index">
+        <button :disabled="randNumbers.length == 0" @click="addNumber(index)" v-show="buttunIngrid[index]">Add Number</button>
+        <p v-show="!buttunIngrid[index]">{{listgrid[index]}}</p>
+      </div>
+    </div> 
+    <div class="generate-number">
+      <p v-for="(value,index) in randNumbers" :key="index" :style="CheckStyle(value)">Generate Number #{{index+1}}: {{value}}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.top-container {
+  width: 330px;
+}
+.action-button {
+  padding: 10px;
+  display: flex;
+  gap: 5px;
+}
+.grid-container {
+  display: grid;
+  grid-template-columns: 100px 100px 100px;
+  grid-template-rows: 80px 80px 80px;
+  column-gap: 5px;
+  row-gap: 5px;
+  background-color: green;
+  padding: 10px;
+}
+.grid-item {
+  display: flex;
+  background-color: rgba(255, 255, 255, 0.8);
+  border: 1px solid rgba(0, 0, 0, 0.8);
+  font-size: 30px;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+.generate-number {
+  font-size: 20px;
+}
+.error-message {
+  height: 40px;
+  color: red;
+}
 </style>
