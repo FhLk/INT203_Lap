384fa6f047c50cf79002a1bbaec45d1ad0f7c59e
FhLk <paramat.fhlk@mail.kmutt.ac.th>
2022-03-07 12:04:28 +0700

    Add playground

---
+++ calculator/src/components/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "@vue/reactivity";
+
+const mul=ref();
+const show=ref(false)
+</script>
+
+<template>
+<div>
+    <input type="number" v-model="mul" />
+    <button @click="show=true">Multiple</button>
+    <ul v-show="show">
+        <p v-for="(value,index) in 12" :key="index">{{mul}}x{{value}} = {{mul*value}}</p>
+    </ul>
+</div>
+</template>
+ 
+<style scoped>
+
+</style>
---
+++ calculator/src/components/cal.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { computed, ref } from 'vue';
+
+let num1=ref(0);
+let num2=ref(0);
+let sum=ref(0);
+const arrSum=ref([]);
+
+const Cal=computed(()=>{
+    sum.value=num1.value+num2.value;
+    arrSum.value.push(sum.value);
+    num1.value=0;
+    num2.value=0;
+})
+</script>
+ 
+<template>
+<div>
+    <input type="number" v-model="num1"/>
+    <input type="number" v-model="num2"/>
+</div>
+<button @click="Cal">Cal</button>
+<p v-show="sum!=0">Result : {{Cal}}</p>
+<p v-show="arrSum.length !=0" v-for="(value,index) in arrSum" :key="index">{{index}}:{{value}}</p>
+</template>
+ 
+<style scoped>
+
+</style>
